b1a1d825cbeb29ba57237f6cd599229ec5a51140
mmch3211 <mmch3211@naver.com>
2019-12-02 20:00:42 +0900

    ggg

--- src/mind/model/dao/HealthDAOImpl.java
+++ src/mind/model/dao/HealthDAOImpl.java
@@ -93,7 +93,7 @@ public class HealthDAOImpl implements HealthDAO {
 	public List<GymDTO> selectGymByKeyword(String keyField, String keyword) throws SQLException {
 		Connection con = null;
 		PreparedStatement ps = null;
-		String sql = proFile.getProperty("gym.selectByKeyword");//gym.selectByKeyword=SELECT CODE, NAME, ADDR, PHONE_NUM, FILE_NAME, GYM_CAPACITY, PRICE, GYM_COMMENT, WEEKDAY_HOUR, WEEKEND_HOUR, STAR_SCORE FROM GYM WHERE ? LIKE ?
+		String sql = proFile.getProperty("gym.selectByKeyword");//sql = gym.selectByKeyword=SELECT CODE, NAME, ADDR, PHONE_NUM, FILE_NAME, GYM_CAPACITY, PRICE, GYM_COMMENT, WEEKDAY_HOUR, WEEKEND_HOUR, STAR_SCORE FROM GYM WHERE ? LIKE ?
 		ResultSet rs = null;
 		List<GymDTO> list = new ArrayList<GymDTO>();
 		
@@ -123,27 +123,103 @@ public class HealthDAOImpl implements HealthDAO {
 			}
 			
 		} finally {
-			DbUtil.dbClose(ps, con);
+			DbUtil.dbClose(rs, ps, con);
 		}
 		return list;
 	}
 
 	@Override
 	public GymDTO selectGymByCode(int gymCode) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = proFile.getProperty("gym.selectByCode");//sql = SELECT CODE, NAME, ADDR, PHONE_NUM, FILE_NAME, GYM_CAPACITY, PRICE, GYM_COMMENT, WEEKDAY_HOUR, WEEKEND_HOUR, STAR_SCORE FROM GYM WHERE CODE = ?
+		ResultSet rs = null;
+		GymDTO gym = null;
+		
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, gymCode);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				int code = rs.getInt("CODE");
+				String name = rs.getString("NAME");
+				
+				String addr = rs.getString("ADDR");
+				String phoneNum = rs.getString("PHONE_NUM");
+				String fileName = rs.getString("FILE_NAME");
+				int gymCapacity = rs.getInt("GYM_CAPACITY");
+				int price = rs.getInt("PRICE");
+				String comment = rs.getString("GYM_COMMENT");
+				String weekdayHour = rs.getString("WEEKDAY_HOUR");
+				String weekendHour = rs.getString("WEEKEND_HOUR");
+				double avgScore = rs.getDouble("STAR_SCORE");
+				
+				gym = new GymDTO(code, name, addr, phoneNum, fileName, gymCapacity, price, comment, weekdayHour, weekendHour, avgScore);
+				
+			}
+			
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		return gym;
 	}
 
 	@Override
 	public int insertReview(ReviewDTO review) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = proFile.getProperty("review.insert"); //INSERT INTO REVIEW (CODE, GYM_CODE, MEMBER_ID, REG_DATE, STAR_SCORE, CONTENT, FILE_NAME) VALUES(REVIEW_CODE_SEQ.NEXTVAL, ?, ?, SYSDATE, ?, ?, ?)
+		int result = 0;
+		
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, review.getGymCode());
+			ps.setString(2, review.getMemberId());
+			ps.setDouble(3, review.getStarScore());
+			ps.setString(4, review.getContent()); //여기부터
+			ps.setString(5, review.getFileName());
+
+			result = ps.executeUpdate();
+		} finally {
+			DbUtil.dbClose(ps, con);
+		}
+		return result;
 	}
 
 	@Override
 	public List<ReviewDTO> selectReviewByGymCode(int gymCode) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = proFile.getProperty("gym.selectByKeyword");//SELECT CODE, GYM_CODE, MEMBER_ID, REG_DATE, STAR_SCORE, CONTENT, FILE_NAME FROM REVIEW WHERE GYM_CODE = ?
+		ResultSet rs = null;
+		List<ReviewDTO> list = new ArrayList<ReviewDTO>();
+		
+		try {
+			con = DbUtil.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, gymCode);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				int code = rs.getInt("CODE");
+				String memberId = rs.getString("MEMBER_ID");
+				String regDate = rs.getString("REG_DATE");
+				double starScore = rs.getDouble("STAR_SCORE");
+				String content = rs.getString("CONTENT");
+				String fileName = rs.getString("FILE_NAME");
+				int gCode = rs.getInt("GYM_CODE");	//gymCode가 매개변수로 사용되고 있어서 gCode로 사용!
+				ReviewDTO review = new ReviewDTO(code, memberId, regDate, starScore, content, fileName, gCode);
+				list.add(review);
+			}
+			
+		} finally {
+			DbUtil.dbClose(rs, ps, con);
+		}
+		return list;
 	}
 
 	@Override
