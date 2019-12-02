e50edf499ae77eb57ef96de0ef837836a1f16bd8
KOSTA <KOSTA@192.168.0.113>
2019-12-02 15:08:28 +0900

    service 추가 19/12/02

---
+++ src/mind/model/dao/HealthDAOImpl.java
@@ -0,0 +1,128 @@
+package mind.model.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import mind.model.dto.GymDTO;
+import mind.model.dto.MemberDTO;
+import mind.model.dto.PointDTO;
+import mind.model.dto.ReviewDTO;
+import mind.model.dto.UseDetailDTO;
+
+public class HealthDAOImpl implements HealthDAO {
+
+	@Override
+	public int insertMember(MemberDTO member) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDTO selectMemberById(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateMember(MemberDTO member) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteMember(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean duplicateById(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int updatePoint(String memberId) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PointDTO selectPoint(String memberId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertGym(GymDTO gym) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<GymDTO> selectAllGym() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GymDTO> selectGymByKeyword(String keyField, String keyword) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GymDTO selectGymByCode(int gymCode) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertReview(ReviewDTO review) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<ReviewDTO> selectReviewByGymCode(int gymCode) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateReview(ReviewDTO review) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteReview(int reviewCode) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertUseDetail(UseDetailDTO useDetail) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<UseDetailDTO> selectUseDetailByKeyword(String keyField, String keyword) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UseDetailDTO> selectUseDetailByGymCodeState(int gymCode, int state) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateUseDetail(int useDetailCode, int state) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/mind/service/HealthService.java
+++ src/mind/service/HealthService.java
@@ -3,119 +3,177 @@ package mind.service;
 import java.sql.SQLException;
 import java.util.List;
 
+import mind.model.dao.HealthDAO;
+import mind.model.dao.HealthDAOImpl;
 import mind.model.dto.GymDTO;
 import mind.model.dto.MemberDTO;
 import mind.model.dto.PointDTO;
 import mind.model.dto.ReviewDTO;
 import mind.model.dto.UseDetailDTO;
 
-public interface HealthService {
-	
-	
-	
-	/**
-	 * 회원가입(유저 등록)
-	 * */
-	int insertMember(MemberDTO member) throws SQLException;
+public class HealthService {
+	private static HealthDAO healthDAO = new HealthDAOImpl();
+		
 	
 	/**
-	 * 아이디로 유저 검색
+	 * 유저 회원가입(등록)
 	 * */
-	MemberDTO selectMemberById(String id) throws SQLException;
+	public static int insertMember(MemberDTO member) throws SQLException{
+		
+		return 0;
+	}
+	
+	/**
+	 * 아이디로유저검색
+	 * */
+	public static MemberDTO selectMemberById(String id) throws SQLException{
+		
+		return null;
+	}
 	
 	/**
 	 * 회원 정보 수정
 	 * */
-	int updateMember(MemberDTO member) throws SQLException;
+	public static int updateMember(MemberDTO member) throws SQLException{
+		
+		return 0;
+	}
 	
 	/**
-	 * 회원탈퇴(유저 삭제)
+	 * 회원 탈퇴(삭제)
 	 * */
-	int deleteMember(String id) throws SQLException;
+	public static int deleteMember(String id) throws SQLException{
+		
+		return 0;
+	}
 	
 	/**
-	 * 아이디 중복체크 (회원가입 할 때 필요)
-	 * @return 중복이면 true 아니면 false
+	 * 아이디 중복체크
+	 * true = 중복 / false = 중복아님
 	 * */
-	boolean duplicateById(String id) throws SQLException;
+	public static boolean duplicateById(String id) throws SQLException{
+		
+		return false;
+	}
 	
 	//////////////////////////////////////////////////////////////////////////
 	
 	/**
 	 * 포인트 잔액 갱신
 	 * */
-	int updatePoint(String memberId) throws SQLException;
+	public static int updatePoint(String memberId) throws SQLException{
+		
+		return 0;
+	}
 	
 	/**
-	 * 포인트 잔액 출력
+	 * 포인트 잔액 검색, 출력
 	 * */
-	PointDTO selectPoint(String memberId) throws SQLException;
+	public static PointDTO selectPoint(String memberId) throws SQLException{
+		
+		return null;
+	}
 	
-	/////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
 	
 	/**
 	 * 헬스장 등록
 	 * */
-	int insertGym(GymDTO gym) throws SQLException;
+	public static int insertGym(GymDTO gym) throws SQLException{
+		
+		return 0;
+	}
 	
 	/**
 	 * 모든 헬스장 검색
 	 * */
-	List<GymDTO> selectAllGym() throws SQLException;
+	public static List<GymDTO> selectAllGym() throws SQLException{
+		
+		return null;
+	}
 	
 	/**
 	 * 헬스장 조건 검색
 	 * */
-	List<GymDTO> selectGymByKeyword(String keyField, String keyword) throws SQLException;
+	public List<GymDTO> selectGymByKeyword(String keyField, String keyword) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	/**
 	 * 헬스장 상세보기에 필요한 검색
 	 * */
-	GymDTO selectGymByCode(int gymCode) throws SQLException;
-	
-	///////////////////////////////////////////////////////////////////////////////
+	public static GymDTO selectGymByCode(int gymCode) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	/**
-	 * 리뷰 등록
+	 * 리뷰등록
 	 * */
-	int insertReview(ReviewDTO review) throws SQLException;
+	public static int insertReview(ReviewDTO review) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	/**
 	 * 헬스장 리뷰 검색(출력)
 	 * */
-	List<ReviewDTO> selectReviewByGymCode(int gymCode) throws SQLException;
+	public static List<ReviewDTO> selectReviewByGymCode(int gymCode) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	/**
 	 * 리뷰 수정
 	 * */
-	int updateReview(ReviewDTO review) throws SQLException;
+	public static int updateReview(ReviewDTO review) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	/**
 	 * 리뷰 삭제
 	 * */
-	int deleteReview(int reviewCode) throws SQLException;
+	public static int deleteReview(int reviewCode) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	/////////////////////////////////////////////////////////////////////
 	
 	/**
+	 * 이용내역 추가 
 	 * 사용자가 헬스장 이용 버튼을 눌렀을 때 등록(호출)됨
 	 * */
-	int insertUseDetail(UseDetailDTO useDetail) throws SQLException;
+	public static int insertUseDetail(UseDetailDTO useDetail) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	/**
-	 * 사용자의 이용내역
+	 * 사용자의 이용내역 검색
 	 * */
-	List<UseDetailDTO> selectUseDetailByKeyword(String keyField, String keyword) throws SQLException;
+	public static List<UseDetailDTO> selectUseDetailByKeyword(String keyField, String keyword) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	/**
-	 * 헬스장 이용 신청을 한 사용자 목록
-	 * @param state는 -1 만료됨, 0 사용됨, 1 사용가능
+	 * 헬스장 이용 신청을 한 사용자 목록 검색
 	 * */
-	List<UseDetailDTO> selectUseDetailByGymCodeState(int gymCode, int state) throws SQLException;
+	public static List<UseDetailDTO> selectUseDetailByGymCodeState(int gymCode, int state) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	/**
 	 * 이용상태 갱신
 	 * */
-	int updateUseDetail(int useDetailCode, int state) throws SQLException;
-
+	public static int updateUseDetail(int useDetailCode, int state) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
